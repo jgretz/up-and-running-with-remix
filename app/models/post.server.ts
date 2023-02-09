8569f2f4ed88ef7f1873150b52bf06116d671ecb
Josh Gretz <jgretz@truefit.io>
2023-02-09 15:40:02 -0500

    finish through video 9

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,5 +1,15 @@
 import { prisma } from "~/db.server";
 
+export async function getPostListings() {
+  return prisma.post.findMany({
+    select: { slug: true, title: true },
+  });
+}
+
 export async function getPosts() {
   return prisma.post.findMany();
+}
+
+export async function getPost(slug: string | undefined) {
+  return prisma.post.findFirst({ where: { slug } });
 }

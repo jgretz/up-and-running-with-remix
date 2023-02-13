a4d17b47192692673caeb439273b3f400b4b53fd
Josh Gretz <jgretz@truefit.io>
2023-02-13 15:15:45 -0500

    through video 16

--- app/routes/posts/admin/new.tsx
+++ app/routes/posts/admin/new.tsx
@@ -1,7 +1,7 @@
 import type { ActionFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
-import { Form, useActionData } from "@remix-run/react";
+import { Form, useActionData, useTransition } from "@remix-run/react";
 import { createPost } from "~/models/post.server";
 import invariant from "tiny-invariant";
 
@@ -45,6 +45,10 @@ const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`
 export default function AdminNewRoute() {
   const errors = useActionData<ActionData>();
 
+  const transition = useTransition();
+
+  const isCreating = Boolean(transition.submission);
+
   return (
     <Form method="post">
       <p>
@@ -83,8 +87,9 @@ export default function AdminNewRoute() {
           name="intent"
           value={"create"}
           className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
+          disabled={isCreating}
         >
-          Create Post
+          {isCreating ? "Creating ..." : "Create Post"}
         </button>
       </div>
     </Form>

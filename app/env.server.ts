2a029c24055f6d864ae29f986783d0095c16ac44
Josh Gretz <jgretz@truefit.io>
2023-02-20 17:23:09 -0500

    through video 18

---
+++ app/env.server.ts
@@ -0,0 +1,21 @@
+import invariant from "tiny-invariant";
+
+export function getEnv() {
+  invariant(
+    process.env.ADMIN_EMAIL,
+    "ADMIN_EMAIL env constant should be defined"
+  );
+
+  return {
+    ADMIN_EMAIL: process.env.ADMIN_EMAIL,
+  };
+}
+
+type ENV = ReturnType<typeof getEnv>;
+
+declare global {
+  var ENV: ENV;
+  interface Window {
+    ENV: ENV;
+  }
+}
